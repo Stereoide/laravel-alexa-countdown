b8335eb49897fa771aaaca4c74fccdb49f6340e7
Jörn Maske <j.maske@otterbach.de>
2018-11-01 21:41:11 +0100

    Added develpr/alexa-app package

--- config/app.php
+++ config/app.php
@@ -164,6 +164,8 @@ return [
          * Package Service Providers...
          */
 
+        \Develpr\AlexaApp\Provider\LaravelServiceProvider::class,
+
         /*
          * Application Service Providers...
          */
@@ -222,6 +224,8 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
 
+        'AlexaRoute' => \Develpr\AlexaApp\Facades\AlexaRouter::class,
+        'Alexa' => \Develpr\AlexaApp\Facades\Alexa::class,
     ],
 
 ];
